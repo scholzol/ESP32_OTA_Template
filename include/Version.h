58842a6269bb37026cf34b91a8aa884b967f3041
olaf scholz <olaf.scholz@online.de>
2023-01-16 15:43:13 +0100

    version.h changed

--- include/Version.h
+++ include/Version.h
@@ -1,4 +1,4 @@
-#define SemanticVersion "0.2.0-feature1.1"
+#define SemanticVersion "0.2.0-alpha.9"
 #define MajorVersion (0)
 #define MinorVersion (2)
-#define SHA_short "0326b29"
+#define SHA_short "df1ad0e"
