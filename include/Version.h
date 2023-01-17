7da4986db0c96f4393e21da21f70ee5388168a3d
olaf scholz <olaf.scholz@online.de>
2023-01-17 22:17:40 +0100

    webserver added

--- include/Version.h
+++ include/Version.h
@@ -1,4 +1,4 @@
-#define SemanticVersion "0.2.0-alpha.9"
+#define SemanticVersion "0.2.0-webserver.1"
 #define MajorVersion (0)
 #define MinorVersion (2)
-#define SHA_short "df1ad0e"
+#define SHA_short "58842a6"
