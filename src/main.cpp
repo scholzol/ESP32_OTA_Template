5768dcce88206afcbd43ebde962a11b5a11d6f77
olaf scholz <olaf.scholz@online.de>
2023-04-26 15:00:14 +0200

    webclient() gets own function

--- src/main.cpp
+++ src/main.cpp
@@ -181,6 +181,72 @@ void updateBoardTable(char ssid32[13])
         MYSQL_DISPLAY("\nConnect failed. Trying again on next iteration.");
     }
 }
+
+void WebClient() {
+// initialize WebClient
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            // Web Page Heading
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            // Feel free to change style properties to fit your preferences
+            client.println("<style>");
+            client.println("html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println("h1 {background-color: powderblue;}");
+            client.println("</style>");
+            client.println("</head>");
+
+            // Web Page body
+            client.println("<body>");
+            client.println("<h1>Board info</h1>");
+            client.println("<p>Semantic Version: " + String(SemanticVersion) + "</p>");
+            client.println("<p>short commit SHA: " + String(SHA_short) + "</p>");
+            client.println("<p>working directory: " + String(WorkingDirectory) + "</p>");
+            client.println("<p>ChipID: " + String(ssidesp32) + "</p>");
+            client.println("</body>");
+            client.println("</html>");
+
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+  }
+}
 // helper functions end ========================================================================================
 
 
@@ -258,70 +324,7 @@ void loop() {
 // OTA loop ####################################
   ArduinoOTA.handle();
 // OTA loop ####################################
+  WebClient();
 
-// initialize WebClient
-
-  WiFiClient client = server.available();   // Listen for incoming clients
-
-  if (client) {                             // If a new client connects,
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
-      currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            // Web Page Heading
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            // Feel free to change style properties to fit your preferences
-            client.println("<style>");
-            client.println("html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println("h1 {background-color: powderblue;}");
-            client.println("</style>");
-            client.println("</head>");
-
-            // Web Page body
-            client.println("<body>");
-            client.println("<h1>Board info</h1>");
-            client.println("<p>Semantic Version: " + String(SemanticVersion) + "</p>");
-            client.println("<p>short commit SHA: " + String(SHA_short) + "</p>");
-            client.println("<p>working directory: " + String(WorkingDirectory) + "</p>");
-            client.println("<p>ChipID: " + String(ssidesp32) + "</p>");
-            client.println("</body>");
-            client.println("</html>");
-
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-  }
 }
 // loop end =============================================================================================
