80a79385d91cd4ab6eaaa87e11add29a5b9510ef
olaf scholz <olaf.scholz@online.de>
2023-04-26 15:27:47 +0200

    platformio.ini extended

--- include/Version.h
+++ include/Version.h
@@ -1,4 +1,4 @@
 #define SemanticVersion "0.1.2"
-#define SHA_short "219e1f2"
-#define FullBuildMetaData "46.Branch.master.Sha.219e1f2646d7589bcf5f6e5a06cc2ff0f0d27afd"
+#define SHA_short "595fd28"
+#define FullBuildMetaData "51.Branch.master.Sha.595fd2880599402efb27825a6a9eb1a58ce19545"
 #define WorkingDirectory "ESP32_OTA_Template"
